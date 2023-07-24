aa7436a6f2a83fbd48c99f7ea6c5b2ef3555e7e6
Sanyam Goyal <sanyamgoyal2859@gmail.com>
2023-07-24 16:13:02 +0530

    added linkedin profile

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,6 +4,7 @@ import {
     AiFillLinkedin,
     AiFillYoutube,
 } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
     return (
@@ -15,7 +16,9 @@ const Hero = () => {
             </div>
             <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-300'>
                 <AiFillTwitterCircle className='cursor-pointer hover:text-cyan-500' />
+                <a href={'https://www.linkedin.com/in/munish-soni-000a5353/'} target='_blank'>
                 <AiFillLinkedin className='cursor-pointer hover:text-blue-700' />
+                </a>
                 <AiFillYoutube className='cursor-pointer hover:text-red-600' />
             </div>
             <div className='mx-auto bg-gradient-to-b from-teal-500 rounded-full mt-10 overflow-hidden h-48 w-48 sm:h-80 sm:w-80 md:h-96 md:w-96'>
